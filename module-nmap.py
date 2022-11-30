e9da2e2a36dfa596a9be8085b9b3b47b14ee34a6
Foufou-exe <thibaut.maurras34@gmail.com>
2022-11-30 23:48:00 +0100

    New Files

--- module-nmap.py
+++ module-nmap.py
@@ -1,9 +1,9 @@
-import main 
+import nmap 
 
 reponse = input(str("Enter the IP address: "))
 
 def nmap_host_all(host):
-    nm = main.PortScanner()
+    nm = nmap.PortScanner()
     nm.scan(host,'0-65535')
     if nm[host].state()== 'up':
         return nm[host]['tcp']
