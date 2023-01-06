5dad59c1bd042ab3d7b3bfbe966fbfe1731746bb
Foufou-exe <thibaut.maurras34@gmail.com>
2023-01-06 19:40:59 +0100

    PATH: Update code for API-SEMABOX

--- Application/web/app.py
+++ Application/web/app.py
@@ -4,27 +4,47 @@ import sys
 sys.path.append('Application')
 from modules.info_server import get_ip_address as ip_address
 import ast
+import json
 
 app = Flask(__name__, template_folder='template')
 
 @app.route('/api/<script>', methods=['POST'])
 def create_script(script):
-    script = request.get_json()['script']
-    result = execute_script(f'Application/modules/{script}')
-    return jsonify(result=result)
+    data = request.get_json()
+    script = data['script']
+    result = subprocess.run(['python', f'Application/modules/{script}'], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    liste = ast.literal_eval(output)
+    if liste is None or not isinstance(liste, dict):
+        return "Aucune information sur le serveur disponible"
+    json_string = json.dumps(liste)
+    return jsonify(result=json_string)
 
 @app.route('/api/<script>', methods=['GET'])
 def get_script(script):
-    # récupérez le nom du script
-    script = request.get_json()['script']
-    result = execute_script(f'Application/modules/{script}')
-    return jsonify(result=result)
+    data = request.get_json()
+    script = data['script']
+    result = subprocess.run(['python', f'Application/modules/{script}'], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    liste = ast.literal_eval(output)
+    if liste is None or not isinstance(liste, dict):
+        return "Aucune information sur le serveur disponible"
+    json_string = json.dumps(liste)
+    return jsonify(result=json_string)
+
 
 @app.route('/api/<script>', methods=['PUT'])
 def update_script(script):
-    script = request.get_json()[f'Application/modules/{script}']
-    result = execute_script(script)
-    return jsonify(result=result)
+    data = request.get_json()
+    script = data['script']
+    result = subprocess.run(['python', f'Application/modules/{script}'], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    liste = ast.literal_eval(output)
+    if liste is None or not isinstance(liste, dict):
+        return "Aucune information sur le serveur disponible"
+    
+    json_string = json.dumps(liste)
+    return jsonify(result=json_string)
 
 
 @app.route('/api/test')
@@ -37,6 +57,8 @@ def execute_script():
     
     if info_server is None or not isinstance(info_server, dict):
         return "Aucune information sur le serveur disponible"
+    
+    
     # Utilisez la fonction render_template de Jinja2 pour afficher le résultat en HTML
     return render_template('info_server.html', info_server=info_server.items())
 
